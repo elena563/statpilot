4f2fc157847d7f33ed106284dea14efcacf3883a
Elena Zen <elenazen563@gmail.com>
2025-05-25 18:59:43 +0200

    End learn page, fix eventlisteners error

--- static/script.js
+++ static/script.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const form = document.querySelector('form');
             const spinner = document.getElementById('spinner');
             
-            form.addEventListener('submit', function() {
-                spinner.style.display = 'block';
-            });
+            if (form){   /* due to forms dynamism, check element presence before adding event listener */
+                form.addEventListener('submit', function() {
+                    spinner.style.display = 'block';
+                });
+            }
         });
 
 /*interactive form for modeling section*/      
@@ -14,26 +16,19 @@ const classif = document.getElementById("classif");
 const regr = document.getElementById("regr");
 const classOpt = document.getElementById("classif-models");
 const regrOpt = document.getElementById("regr-models");
-
-classif.addEventListener("change", () => {
-    if (classif.checked) {
-        classOpt.style.display = "flex";
-        regrOpt.style.display = "none";
-    }
-});
-
-regr.addEventListener("change", () => {
-    if (regr.checked) {
-        regrOpt.style.display = "flex";
-        classOpt.style.display = "none";
-    }
-});
-
-/*modeling options
-function toggleModels() {
-    classif = document.getElementById('classif').checked
-    regr = document.getElementById('regr').checked
-
-    document.getElementById('classif-models').display = classif ? 'block' : 'none'
-    document.getElementById('regr-models').display = regr ? 'block' : 'none'
-}*/
+if (classif){   
+    classif.addEventListener("change", () => {
+        if (classif.checked) {
+            classOpt.style.display = "flex";
+            regrOpt.style.display = "none";
+        }
+    });
+}
+if (regr){
+    regr.addEventListener("change", () => {
+        if (regr.checked) {
+            regrOpt.style.display = "flex";
+            classOpt.style.display = "none";
+        }
+    });
+}
